d00b176614b27f0c3fd777c3dd4ca7a4214cd68d
jurusteve <jstevemu17@gmail.com>
2019-04-12 14:03:28 -0400

    completed mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getSmurfs, moreSmurfs } from '../actions'
+import AddSmurf from './AddSmurf'
+import Smurf from './Smurf'
+import './App.css'
 /*
  to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
+ I'll let you do this part on your own.
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      formActive: false
+    }
+  }
+  componentDidMount () {
+    this.props.getSmurfs()
+  }
+
+  toggleForm = ()=>{
+    this.setState({formActive: !this.state.formActive})
+  }
+
+  render () {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <div className='App'>
+        <h1>SMURFS! 2.0</h1>
+        <div className='add-smurf-button'>
+          <button onClick={()=> this.toggleForm()}>Add a Smurf</button>
+          {this.state.formActive
+            &&
+            <AddSmurf 
+            isAdding={this.props.addingSmurf}
+            moreSmurfs={this.props.moreSmurfs}
+            toggle={this.toggleForm}
+            />
+        }
+        </div>
+        <div className='main'>
+        {
+          !this.state.formActive && 
+          <Smurf smurfs={this.props.smurfs} />
+        }
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs,
+  error: state.error,
+  fetchingSmurfs: state.fetchingSmurfs,
+  addingSmurf: state.addingSmurf,
+  updatingSmurf: state.updatingSmurf,
+  deletingSmurf: state.deletingSmurf
+})
+
+export default connect(mapStateToProps, { getSmurfs, moreSmurfs })(App)
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,22 +1,71 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import { GET_SMURFS,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILURE,
+  MORE_SMURFS,
+  MORE_SMURFS_SUCCESS,
+  MORE_SMURFS_FAILURE } from '../actions'
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+ */
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ''
+      }
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+        error: ''
+      }
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    case MORE_SMURFS:
+      return {
+        ...state,
+        addingSmurfs: true,
+        error: ''
+      }
+    case MORE_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        addingSmurfs: false,
+        error: ''
+      }
+    case MORE_SMURFS_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    default:
+      return state
+  }
+}
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
